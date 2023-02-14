f6cdb831a47aaba94aa72fa2b54acec7ceeb7dd5
Данила Тихонов <danilatihonov@MacBook-Air--Danila.local>
2023-02-14 21:03:08 +0530

    Создание программы на умножение двух чисел

---
+++ Умножение двух чисел/Program.cs
@@ -0,0 +1,12 @@
+﻿double x;
+double y;
+double result;
+
+Console.WriteLine("Впишите число Х: ");
+x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Впишите число Y: ");
+y = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ваш результат: ");
+Console.WriteLine(result = x*y);
