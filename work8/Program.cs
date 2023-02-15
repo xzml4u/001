5928f216bac07e79fda130df534b6af6d4e11c32
Данила Тихонов <danilatihonov@MacBook-Air--Danila.local>
2023-02-15 21:15:08 +0530

    work 8 - Задача 8

---
+++ work8/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = 5;
+int evenNumber = 2;
+
+
+if(number > 1){
+    while(evenNumber <= number){
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
